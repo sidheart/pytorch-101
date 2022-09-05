8122018c59cb4e67471d7ff00ddb31a0092b8e73
Sid Narayan <sidsdn@protonmail.com>
2022-09-04 23:20:02 -0700

    Update neural_network tutorial

--- tutorials/neural_network.py
+++ tutorials/neural_network.py
@@ -22,13 +22,16 @@ labels_map = {
 # my machine has cuda ;)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# ReLU means rectified linear activation function
-# if you aren't a fucking nerd it just means F(x) = max(0, x)
+# Flatten takes the 2D image and lays its pixels out in a 1D contiguous array
+# Sequential applies a sequence of transforms in order
 # Each Linear(x, y) step takes x input features and outputs a tensor of y output features
+# ReLU means rectified linear activation function if you aren't a fucking nerd it just means F(x) = max(0, x)
+# This class doesn't do shit, but if you train it on the dataset it eventually will
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
+        self.flatten = nn.Flatten() 
+        # Sequential just applys t
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(28*28, 512),
             nn.ReLU(),
@@ -44,7 +47,6 @@ class NeuralNetwork(nn.Module):
 
 # Define the model and put it on the GPU
 model = NeuralNetwork().to(device)
-print(model)
 
 # Use the model to get a prediction
 x = torch.rand(1, 28, 28, device=device)
@@ -52,3 +54,8 @@ logits = model(x)
 pred_probab = nn.Softmax(dim=1)(logits)
 y_pred = pred_probab.argmax(1)
 print(f"Predicted class: {labels_map[y_pred.item()]}")
+
+# Each model is built of layers and many layers are parameterized, i.e. have associated weights and biases that are optimized during training
+print(f"Model structure: {model}\n\n")
+for name, param in model.named_parameters():
+    print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
